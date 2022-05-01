ee23ce76f565a84ecab63d37bf6eb6eb6316da75
ishmamabid99 <ishmam.abid5422@gmail.com>
2022-05-01 14:16:52 +0600

    Reverted to Mui  4.3.2

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express  = require('express');
-const mongoose = require('moongoose');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
